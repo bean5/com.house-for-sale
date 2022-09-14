c2ae66b3e4d98c7e1b0331a3dda8799ee3869b86
bean5 <2052646+bean5@users.noreply.github.com>
2022-09-14 08:57:54 -0600

    Revert "wip flexible column"
    This reverts commit 49a8c6924d6b696fa1b2a5dd1af2a7d196d2357d.

--- component/house-for-sale.js
+++ component/house-for-sale.js
@@ -1,60 +1,49 @@
 import css from './house-for-sale.module.css'
 import HouseSlider from './house-slider'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 export default function HouseForSale() {
   return (
     <div className={css.body}>
       <h1>Beautiful Rambler for Sale by Owner</h1>
       <h2>Deep cleaned, prepared, and bright!</h2>
-      <i>Attractive rambler in established, friendly neighborhood on nearly 0.25 acres, nestled in peaceful cul-de-sac with quick commute.</i>
-      <Container fluid>
-        <Row>
-          <Col>
-            <ul>
-              <li>Short Commutes!</li>
-              <ul>
-                <li>18 minutes to Southworth Fast Ferry</li>
-                <li>6 minutes to library</li>
-                <li>5 minutes to delightful dog park</li>
-                <li>5 minutes to the US post office</li>
-                <li>2 minutes to calm kayaking</li>
-                <li>2 minutes to Starbucks</li>
-                <li>2 minutes to grocery store and banks and restaurants</li>
-              </ul>
-              <li>Amazing non-well water with strong pressure and refreshing taste</li>
-              <li>Repurposed vintage wood gate</li>
-              <li>Green per-room electric heating</li>
-              <li>Modern LED lighting</li>
-              <li>Dog-friendly premium scratch resistant plank flooring</li>
-              <li>Walk in closet</li>
-              <li>Established community</li>
-              <li>Relaxing cross breeze through the home</li>
-              <li>Deep, finished garage with attic access</li>
-              <li>Thick steel reinforced concrete driveway designed and built for parking a 36 foot motorhome</li>
-              <li>There is also ample parking for you and your guests</li>
-              <li>ADA friendly front door pathway, covered porch</li>
-              <li>Bright kitchen</li>
-              <li>Remote controlled fan</li>
-              <li>Archway feature overlooking wide</li>
-              <li>Beautiful views of sunrise and double rainbows over lush garden and visits of wildlife ranging from deer to eagles to bunnies</li>
-              <li>New roof with transferable warranty, newer energy efficient windows, new floors, neutral paint colors. fresh paint</li>
-              <li>There are no houses visible from the home looking out into the back yard giving you privacy and an experience of nature</li>
-              <li>Must see it to experience how bright this house is compared to others in this area!! Back yard is secluded and private.</li>
-              <li>Reliable, fast internet. A programmer worked from home here and commuted to Seattle by ferry.</li>
-              <li>HOA: None! Growing neighborhood with recently remodeled homes.</li>
-              <li>Rental income: $2000+</li>
-            </ul>
-            <p><strong>Bonuses</strong>: blackberry bushes, mature trees, fridge, oven, dishwasher, front loading washer dryer set, amateur radio antenna post, nearby elementary school, churches, and open fields. Walking distance to shopping centers. No HOA. Retired neighbor next door who maintains his property like clock work.</p>
-          </Col>
-          <Col>
-            <HouseSlider />
-          </Col>
-        </Row>
-      </Container>
+      <HouseSlider />
       <br />
+      <i>Attractive rambler in established, friendly neighborhood on nearly 0.25 acres, nestled in peaceful cul-de-sac with quick commute.</i>
+      <ul>
+        <li>Short Commutes!</li>
+        <ul>
+          <li>18 minutes to Southworth Fast Ferry</li>
+          <li>6 minutes to library</li>
+          <li>5 minutes to delightful dog park</li>
+          <li>5 minutes to the US post office</li>
+          <li>2 minutes to calm kayaking</li>
+          <li>2 minutes to Starbucks</li>
+          <li>2 minutes to grocery store and banks and restaurants</li>
+        </ul>
+        <li>Amazing non-well water with strong pressure and refreshing taste</li>
+        <li>Repurposed vintage wood gate</li>
+        <li>Green per-room electric heating</li>
+        <li>Modern LED lighting</li>
+        <li>Dog-friendly premium scratch resistant plank flooring</li>
+        <li>Walk in closet</li>
+        <li>Established community</li>
+        <li>Relaxing cross breeze through the home</li>
+        <li>Deep, finished garage with attic access</li>
+        <li>Thick steel reinforced concrete driveway designed and built for parking a 36 foot motorhome</li>
+        <li>There is also ample parking for you and your guests</li>
+        <li>ADA friendly front door pathway, covered porch</li>
+        <li>Bright kitchen</li>
+        <li>Remote controlled fan</li>
+        <li>Archway feature overlooking wide</li>
+        <li>Beautiful views of sunrise and double rainbows over lush garden and visits of wildlife ranging from deer to eagles to bunnies</li>
+        <li>New roof with transferable warranty, newer energy efficient windows, new floors, neutral paint colors. fresh paint</li>
+        <li>There are no houses visible from the home looking out into the back yard giving you privacy and an experience of nature</li>
+        <li>Must see it to experience how bright this house is compared to others in this area!! Back yard is secluded and private.</li>
+        <li>Reliable, fast internet. A programmer worked from home here and commuted to Seattle by ferry.</li>
+        <li>HOA: None! Growing neighborhood with recently remodeled homes.</li>
+        <li>Rental income: $2000+</li>
+      </ul>
+      <p><strong>Bonuses</strong>: blackberry bushes, mature trees, fridge, oven, dishwasher, front loading washer dryer set, amateur radio antenna post, nearby elementary school, churches, and open fields. Walking distance to shopping centers. No HOA. Retired neighbor next door who maintains his property like clock work.</p>
     </div>
   )
 }
