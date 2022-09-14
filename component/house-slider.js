eeb6a07dbb88db1f3fe236f32d1baca212a901c2
bean5 <2052646+bean5@users.noreply.github.com>
2022-09-14 08:48:20 -0600

    Add local image

--- component/house-slider.js
+++ component/house-slider.js
@@ -1,7 +1,9 @@
 import SimpleImageSlider from "react-simple-image-slider";
 
 const images = [
-  { url: "https://lh3.googleusercontent.com/pw/AL9nZEVtl-VNKAuYVXerGXiE77Mgt74NsCpuD78okC11XE31uccg7nxxC9kuvR9_hxX_Rl22emxGaM1m4avejv6jh4eP1SoM7qcGz8aCJbxlglUJjKNUEyr5JfgnrCoV3OVyJabg783AL7VnEUS5aad1VFHJAg=w1600-h1200-no" },
+  // local
+  { url: "../images/PXL_20220905_181904815.MP.jpg" },
+  // remote
   { url: "https://lh3.googleusercontent.com/pw/AL9nZEVtl-VNKAuYVXerGXiE77Mgt74NsCpuD78okC11XE31uccg7nxxC9kuvR9_hxX_Rl22emxGaM1m4avejv6jh4eP1SoM7qcGz8aCJbxlglUJjKNUEyr5JfgnrCoV3OVyJabg783AL7VnEUS5aad1VFHJAg=w1600-h1200-no" },
   { url: "https://lh3.googleusercontent.com/pw/AL9nZEVtl-VNKAuYVXerGXiE77Mgt74NsCpuD78okC11XE31uccg7nxxC9kuvR9_hxX_Rl22emxGaM1m4avejv6jh4eP1SoM7qcGz8aCJbxlglUJjKNUEyr5JfgnrCoV3OVyJabg783AL7VnEUS5aad1VFHJAg=w1600-h1200-no" },
   { url: "https://lh3.googleusercontent.com/pw/AL9nZEVtl-VNKAuYVXerGXiE77Mgt74NsCpuD78okC11XE31uccg7nxxC9kuvR9_hxX_Rl22emxGaM1m4avejv6jh4eP1SoM7qcGz8aCJbxlglUJjKNUEyr5JfgnrCoV3OVyJabg783AL7VnEUS5aad1VFHJAg=w1600-h1200-no" },
