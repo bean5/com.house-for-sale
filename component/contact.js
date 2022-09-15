8002100da4cd3e2fe88627684ddf32f57bd159db
bean5 <2052646+bean5@users.noreply.github.com>
2022-09-15 12:14:13 -0600

    Embed form, add contact information to header

--- component/contact.js
+++ component/contact.js
@@ -4,21 +4,7 @@ export default function Contact() {
   return (
     <div className={css.body}>
       <br />
-      <a href="https://docs.google.com/forms/d/1Ld7u8NdlGmcYdFJHWRldtKQpA6aOHzKqb7M5__zL-h8">Online form</a>
-      <dl>
-        <dt><strong>Cell</strong> <a href="tel:360.689.0181">360.689.0181</a></dt>
-        <dt>&nbsp;</dt>
-        {/* <dt><strong>Mailing address</strong> </dt>
-        <dd>623 Tufts Ave. East, Port Orchard WA 98366-4023</dd>
-        <dt>&nbsp;</dt> */}
-        <dt><strong>Email</strong> <a href="mailto:info@suv123.com">info@suv123.com</a></dt>
-        {/* <dt>&nbsp;</dt>
-        <dt><strong>SPOOF mail</strong> </dt>
-        <dd>
-          <p>Note that someone has a program generating random names that appear to come from SUV123.com.  The junk mail advertises stocks, incoherent messages, and just plain junk.  These unwanted emails are coming from servers as far away as Australia and Europe.  It looks like a pervasive program they have created, but we have been working with our web administrators to put a stop to it.  It is too bad that someone has the time and resources to waste other people's time and web bandwidth with trash.  Please bear with us as we work to shut them down.  If you know how to shut these crooks down, please send an email to the above address.</p>
-          <p>We do <i>not</i> send out bulk email or solicitations.</p>
-        </dd> */}
-      </dl>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBcxkA1u0iui6Og3IDHws0pL4I1yz79gqAIW_CjadPWUEniQ/viewform?embedded=true" width="840" height="1286" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   )
 }
--- component/navbar.js
+++ component/navbar.js
@@ -4,17 +4,12 @@ export default function NavBar() {
   return (
     <div className={css.body}>
       <nav className={css.nav}>
-        {/* <a href="/"><img src="images/logo2.jpg" alt="image of a vehicle ascending a steep slope" border="0" width="105" height="95" /></a> */}
         <ul>
           <li><a href="/">House for Sale</a></li>
-          {/* <li><a href="mission">Mission</a></li>
-          <li><a href="lessons">Lessons</a></li> */}
-          <li><a href="contact">Contact</a></li>
+          <li><a href="contact">Personal Tour</a></li>
+          <li><a href="tel:3606890181">(360) 689-0181</a></li>
+          <li><a href="mailto:info@suv123.com">email</a></li>
         </ul>
-        <p></p>
-        {/* <img src="images/titlea.gif" alt="suv" border="0" width="172" height="125" />
-        <img src="images/titleb.gif" alt="123" border="0" width="141" height="125" />
-        <p className={css.slogan}>Off Road Instruction for New and Experienced Sport Utility Vehicle (SUV) owners.</p> */}
       </nav>
     </div>
   )
