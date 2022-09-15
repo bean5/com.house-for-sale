091db423ca6e606a586f04986953a8e56e7235b8
bean5 <2052646+bean5@users.noreply.github.com>
2022-09-15 11:35:55 -0600

    Slim down menu and footer

--- component/footer.js
+++ component/footer.js
@@ -4,20 +4,6 @@ import css from './footer.module.css'
 export default function Footer() {
   return (
     <div className={css.body}>
-      <br />
-      <br />
-      <br />
-      <br />
-      <hr />
-      <h4>WE SUPPORT LOCAL SEARCH AND RESCUE TEAMS.</h4>
-      <h5>
-        Send mail to <a href="mailto:service@suv123.com">service@suv123.com</a> with questions or comments.
-        <br />
-        Copyright Ⓒ 1999-2022 SUV123
-        <br />
-        Last modified:
-        September 14, 2022
-      </h5>
     </div>
   )
 }
--- component/navbar.js
+++ component/navbar.js
@@ -4,17 +4,17 @@ export default function NavBar() {
   return (
     <div className={css.body}>
       <nav className={css.nav}>
-        <a href="/"><img src="images/logo2.jpg" alt="image of a vehicle ascending a steep slope" border="0" width="105" height="95" /></a>
+        {/* <a href="/"><img src="images/logo2.jpg" alt="image of a vehicle ascending a steep slope" border="0" width="105" height="95" /></a> */}
         <ul>
-          <li><a href="house-for-sale">House for Sale</a></li>
-          <li><a href="mission">Mission</a></li>
-          <li><a href="lessons">Lessons</a></li>
+          <li><a href="/">House for Sale</a></li>
+          {/* <li><a href="mission">Mission</a></li>
+          <li><a href="lessons">Lessons</a></li> */}
           <li><a href="contact">Contact</a></li>
         </ul>
         <p></p>
-        <img src="images/titlea.gif" alt="suv" border="0" width="172" height="125" />
+        {/* <img src="images/titlea.gif" alt="suv" border="0" width="172" height="125" />
         <img src="images/titleb.gif" alt="123" border="0" width="141" height="125" />
-        <p className={css.slogan}>Off Road Instruction for New and Experienced Sport Utility Vehicle (SUV) owners.</p>
+        <p className={css.slogan}>Off Road Instruction for New and Experienced Sport Utility Vehicle (SUV) owners.</p> */}
       </nav>
     </div>
   )
