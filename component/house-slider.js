57db371dd9a8beca6aad5b0ad10ec3e6b4be6986
bean5 <2052646+bean5@users.noreply.github.com>
2022-09-16 09:44:07 -0600

    Change order of photos

--- component/house-slider.js
+++ component/house-slider.js
@@ -4,10 +4,10 @@ const images = [
   // local
   // { url: "../images/pretty-home-qr.png" },
   // remote
-  { url: "https://lh3.googleusercontent.com/pw/AL9nZEX5e8sOdkA_Uj2QGzrZSVo32Cnr0zrpGKRCVSR_BRdqqvI7pMrycT8JUy0_hZH67S3tdb1eYeybvUsgKo2qUTHQuAhuHLpXACrxkPI6UqkJfqt-4wuZNDPEcJwWyLEc1Oc2QXHx9OrRcVnBBETW_Y9ioA=w920-h690-no?authuser=0" },
+  { url: "https://lh3.googleusercontent.com/pw/AL9nZEWNruV_Zt8U_Rbtak1oqNRGer6SJb4UvBBE7YC3acTJrr-zVqHe02by-xsxTSCqG-S7P3VlQTuqVD0GjXT1hZaK5XEWuoBy0n5R0w9xmL5VlxG_ykoKjQXuaonS0zD1KlpAtb6NKgF5KGxpRMgtxuR33g=w920-h690-no?authuser=0" },
   { url: "https://lh3.googleusercontent.com/pw/AL9nZEX3djJkRgGDzyurmV-n1oEOXtldPOEy_cREPtOdLGNgT-2XkQkqc6J8Ye_J4D0rRg5873UpdchHmS_n9L8Yn9IEW_7oYgqVsDYjIx91JcZ3qwO83wvHR4fj-1miw62BhxOK_jlSnTh7J932_sgW1yvfkw=w920-h690-no?authuser=0" },
   { url: "https://lh3.googleusercontent.com/pw/AL9nZEUVnf3TLp8Gu_WAgrAvikToc6CgYBgPKS97fzd4mKRJN45Z35RBSDf4tGt7DykNHn9fbeuxqpSJESRRxoPbEd3IxXfMN2xUZsybbkKm-Ru2uZTc58xBpP44b2VPc_ZiXapp3Vy00Dq5JEA5PPBOouXF5g=w920-h690-no?authuser=0" },
-  { url: "https://lh3.googleusercontent.com/pw/AL9nZEWNruV_Zt8U_Rbtak1oqNRGer6SJb4UvBBE7YC3acTJrr-zVqHe02by-xsxTSCqG-S7P3VlQTuqVD0GjXT1hZaK5XEWuoBy0n5R0w9xmL5VlxG_ykoKjQXuaonS0zD1KlpAtb6NKgF5KGxpRMgtxuR33g=w920-h690-no?authuser=0" },
+  { url: "https://lh3.googleusercontent.com/pw/AL9nZEX5e8sOdkA_Uj2QGzrZSVo32Cnr0zrpGKRCVSR_BRdqqvI7pMrycT8JUy0_hZH67S3tdb1eYeybvUsgKo2qUTHQuAhuHLpXACrxkPI6UqkJfqt-4wuZNDPEcJwWyLEc1Oc2QXHx9OrRcVnBBETW_Y9ioA=w920-h690-no?authuser=0" },
   // { url: "https://lh3.googleusercontent.com/pw/AL9nZEV5WI5VCZlsOSOXkhFB-zrW-RBN7K49_6HWqy9BWKDHb0xMEaFvA5A8jy6c1y9sGohl2hzYRX57fKcz4-HnpB8FZPvednZ-tVuPIoUnkfVz_2d1N3_zWnQQj9Kf9LzXFcKmOapuhNo-tJ-Zshz5NiDoxQ=w920-h690-no?authuser=0" },
   { url: "https://lh3.googleusercontent.com/pw/AL9nZEXSB6uvGwmCNg7gi9puNJ1yvEsbJyCIgvaRKvJowHeg5brSgbGaSEEYm0g8_699RwOXW9xDeHm0IM3ORseTbWt062w-FeJetg3u1P2UHKnn03fcrG-XwFXt5bBfvFUpO198zkrlvfQXV3RSxT5QGwHs5g=w920-h1227-no?authuser=0" },
   // { url: "https://lh3.googleusercontent.com/pw/AL9nZEV9O3YeEHADIWDAqLJzbo0R38jZYkFKWRv_QEWC9h4ui297SGns1nCYlymfW2e2rtfi79_2OHs4MGK_2-o9-yy1IRSIbBsqdOIJs6aaj7Gw8u5mbrR8zqbyN1-ArGlV5-r9YVem0u3adBYvw8Wh5GYv_A=w920-h1227-no?authuser=0" },
