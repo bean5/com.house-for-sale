10c5a1a6dbf3b80d27a0290e3e266547a6aa8807
bean5 <2052646+bean5@users.noreply.github.com>
2022-09-16 09:55:13 -0600

    Rephrase menu items

--- component/navbar.js
+++ component/navbar.js
@@ -5,8 +5,8 @@ export default function NavBar() {
     <div className={css.body}>
       <nav className={css.nav}>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="contact">Tour</a></li>
+          <li><a href="/">Photos</a></li>
+          <li><a href="contact">Contact</a></li>
           {/* <li><a href="tel:3606890181">(360) 689-0181</a></li>
           <li><a href="mailto:info@suv123.com">email</a></li> */}
         </ul>
