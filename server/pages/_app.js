552d8eab4bbb42d500191122d107ef57db410d77
bean5 <2052646+bean5@users.noreply.github.com>
2024-04-25 12:34:06 -0600

    Updates

---
+++ server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var r={};r.id=888,r.ids=[888],r.modules={4342:(r,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>u});var s=t(997);t(3118);let u=function({Component:r,pageProps:e}){return s.jsx(r,{...e})}},3118:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var t=e(e.s=4342);module.exports=t})();
